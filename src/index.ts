911c710e1eb584b34bd9290725e23f986b0c1957
Servio Palacios <servio@palacios.com>
2024-03-17 18:51:06 -0600

    [fixing libraries versions]

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 // src/index.ts
 import * as xlsx from 'xlsx';
-import * as path from 'path';
+//import * as path from 'path';
 
 export function readExcelData(filePath: string): Record<string, any>[] {
-    const workbook = xlsx.readFile(path.resolve(__dirname, filePath));
-    const sheetName = workbook.SheetNames[0];
-    const worksheet = workbook.Sheets[sheetName];
-    const data: Record<string, any>[] = xlsx.utils.sheet_to_json(worksheet);
-    return data;
+    // const workbook = xlsx.readFile(path.resolve(__dirname, filePath));
+    // const sheetName = workbook.SheetNames[0];
+    // const worksheet = workbook.Sheets[sheetName];
+    const _datum = { name: "servio", lastName: "palacios" };
+    let _data: Record<string, any>[] = [];
+    _data.push(_datum);
+
+    //const data: Record<string, any>[] = xlsx.utils.sheet_to_json(worksheet);
+    //const data: Record<string, any>[] = _data;
+    console.log(_data);
+    return _data;
 }
